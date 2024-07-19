0b52d5d656f111d56c8940f1bcdaf6105b05e0c4
FaisalWant <heliconrngr@gmail.com>
2024-07-19 18:54:29 +0530

    Added suffix Trie

---
+++ suffixTrie.py
@@ -0,0 +1,37 @@
+class SuffixTrie: 
+    def __init__(self, string): 
+        self.root={} 
+        self.endSymbol="*" 
+        self.populateSuffixTreeFrom(string) 
+    
+
+    def populateModifiedSuffixTrieFrom(self, string): 
+        for i in range(len(string)): 
+            self.insertSubStringAt(i, string)
+        
+
+    def insertSubStringAt(self, i, string): 
+        node= self.root 
+
+        for idx in range(i, len(string)-1): 
+            letter= string[idx] 
+
+            if letter not in node: 
+                node[letter]= {} 
+            
+            node= node[letter] 
+        
+        node[self.endSymbol] = True 
+    
+
+
+    def contains(self, string): 
+        for letter in string: 
+            if letter not in node: 
+                return False 
+            
+            node= node[letter] 
+        
+        return self.endSymbo in node 
+    
+
